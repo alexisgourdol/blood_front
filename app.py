c548fadd1c15c6d38d08df9140fe3062e869c761
Alexis Gourdol <alexis.gourdol@gmail.com>
2021-12-08 18:35:19 +0000

    read png file, could send bytes directly to API

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
+import io
 
-st.write("hello")
+
+st.title("Blood sample analysis")
+uploaded_file = st.file_uploader("Upload a blood sample .png image")
+
+st.image(uploaded_file)
+
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    # st.write(bytes_data)
+
+
+st.write(np.frombuffer(bytes_data, dtype=np.uint8))
+# reshape necessary
