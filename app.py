76b6b17631d9639fad0bca85e2e0802aa34cda76
Alexis Gourdol <alexis.gourdol@gmail.com>
2021-12-09 09:43:33 +0000

    from image to np array

--- app.py
+++ app.py
@@ -5,15 +5,21 @@ import io
 
 
 st.title("Blood sample analysis")
-uploaded_file = st.file_uploader("Upload a blood sample .png image")
+uploaded_file = st.file_uploader("Upload a blood sample .png image", type=["png"])
 
-st.image(uploaded_file)
 
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    # st.write(bytes_data)
 
+    st.markdown("""### To show uploaded in streamlit""")
+    st.image(uploaded_file)
 
-st.write(np.frombuffer(bytes_data, dtype=np.uint8))
-# reshape necessary
+    # st.markdown("""### To read file as bytes""")
+    # bytes_data = uploaded_file.getvalue()
+    # reshape necessary, but could only get shape with PIL, not from bytes directly
+
+    st.markdown("""### To read file as an image & convert to numpy array""")
+    # image = Image.open(io.BytesIO(bytes_data))
+    image = Image.open(uploaded_file)
+    st.image(image)
+    st.write("Image shape: ", np.asarray(image).shape)
+    st.write(np.asarray(image))
